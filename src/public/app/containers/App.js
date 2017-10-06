084ea2333d074de0a94131665cac8b7e0f13a375
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-06 14:39:25 -0400

    [added] react router

--- src/public/app/containers/App.js
+++ src/public/app/containers/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Render from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Home from '../components/Home';
 import Works from '../components/Works';
@@ -6,13 +8,23 @@ import Resume from '../components/Resume';
 import Contact from '../components/Contact';
 
 const App = () => (
-  <div>
-    <h1>alexis park</h1>
-    <Home />
-    <Works />
-    <Resume />
-    <Contact />
-  </div>
+  <Router>
+    <div>
+      <h1>alexis park</h1>
+      <Route exact path='/' render={() => (
+        <Home />
+      )} />
+      <Route exact path='/works' render={() => (
+        <Works />
+      )} />
+      <Route exact path='/resume' render={() => (
+        <Resume />
+      )} />
+      <Route exact path='/contact' render={() => (
+        <Contact />
+      )} />
+    </div>
+  </Router>
 )
 
 export default App;

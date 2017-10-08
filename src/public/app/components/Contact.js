e6e12d0c83e47a2a3a380d254ef5db53e6e04d15
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-08 14:29:48 -0400

    [added] form to contact component

--- src/public/app/components/Contact.js
+++ src/public/app/components/Contact.js
@@ -5,10 +5,22 @@ import FooterLinks from './FooterLinks';
 class Contact extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+    }
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
   }
   componentDidMount() {
     particlesJS('particles', particleConfig);
     document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
+  handleFirstNameChange(e, field) {
+    const newState = {}
+    newState[field] = e.target.value;
+    this.setState(newState);
   }
   render() {
     return (
@@ -19,6 +31,33 @@ class Contact extends React.Component {
         <div className='page-wrapperBottom'>
           <div className='contact-bottomText page-bottomText'>
             <span>contact..</span>
+            <form className='contact-form' action='' method='post'>
+              <input
+                type='text'
+                placeholder='First Name'
+                value={this.state.firstName}
+                onChange={(e) => this.handleFirstNameChange(e,'firstName')}
+              />
+              <input
+                type='text'
+                placeholder='Last Name'
+                value={this.state.lastName}
+                onChange={(e) => this.handleFirstNameChange(e,'lastName')}
+              />
+              <input
+                type='text'
+                placeholder='Email'
+                value={this.state.email}
+                onChange={(e) => this.handleFirstNameChange(e,'email')}
+              />
+              <input
+                type='text'
+                placeholder='Message'
+                value={this.state.message}
+                onChange={(e) => this.handleFirstNameChange(e,'message')}
+              />
+              <input type='submit' placeholder='save' />
+            </form>
           </div>
         <FooterLinks />
         </div>

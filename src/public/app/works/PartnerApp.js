7fb32b7644bd32ff403261f15c9ae15ddcf4dc75
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-16 22:29:30 -0400

    [added] content

--- src/public/app/works/PartnerApp.js
+++ src/public/app/works/PartnerApp.js
@@ -3,20 +3,71 @@ import FooterLinks from '../components/FooterLinks';
 
 const PartnerApp = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Partner App</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577f30d49f7456bb2e333cc7/1467953373787/?format=1000w" />
 
-            <img src='https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577f30d49f7456bb2e333cc7/1467953373787/?format=1000w' />
+            <p>
+              <strong>Role:</strong> UX/UI
+            </p>
 
-            <p><strong>Role:</strong> UX/UI</p>
+            <p>
+              The app was in its alpha phase when I joined sbux. Much of the
+              foundation was completed by a local creative agency. After I
+              joined, I designed and implemented further features, and did a lot
+              of work to clean up inconsistencies and establish a set styleguide
+              for both iOS and android.
+            </p>
 
-            <p>The app was in its alpha phase when I joined sbux. Much of the foundation was completed by a local creative agency. After I joined, I designed and implemented further features, and did a lot of work to clean up inconsistencies and establish a set styleguide for both iOS and android.</p>
+            <strong>Features implemented:</strong>
 
+            <p>
+              Displays food truck and daily events for Starbucks Support Center
+              (HQ) partners, in place of schedule for retail partners.
+            </p>
+
+            <div>
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577f460cbebafb28867e5710/577f460c6b8f5b3cacc51807/1467958801758/android_foodtruck.png?format=500w" />
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577f460cbebafb28867e5710/577f460cbebafb28867e5711/1467958803690/iOS_foodtruck.png?format=500w" />
+            </div>
+
+            <strong>
+              Nav structure that will allow more items + sub menu items
+            </strong>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/57846f65d1758e87ad4e79bf/1468297399251/?format=1000w" />
+
+            <p>
+              Coffee would expand to reveal further coffee related items (coffee
+              master, coffee passport, 1912 pike)
+            </p>
+
+            <strong>Partner benefits info page</strong>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577f4040893fc07dd846ecb1/1467957338995/?format=1000w" />
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577f406b46c3c4593820247a/1467957375045/?format=500w" />
+
+            <p>Template in action.</p>
+
+            <p>
+              <strong>In the works:</strong>
+            </p>
+            <p class="text-align-center">
+              <strong>Coffee Master</strong>
+            </p>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577f446aff7c50451be3a4e5/1467958385188/?format=1000w" />
+            <p>
+              Supplemental learning material for partners going through the
+              Coffee Master program. Coffee Master certificate is pride and joy
+              of our most dedicated partners. They're always looking for more
+              answers and materials to learn about coffee.
+            </p>
           </div>
         </div>
         <FooterLinks />

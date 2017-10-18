ebd1a17346e3af3b11ddbc065b2013894efde238
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-17 20:11:38 -0400

    [updated] minnovative solutions content

--- src/public/app/works/MinnovativeSolutions.js
+++ src/public/app/works/MinnovativeSolutions.js
@@ -3,16 +3,58 @@ import FooterLinks from '../components/FooterLinks';
 
 const MinnovativeSolutions = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Minnovative Solutions</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f2739ee4b0fe5ea5e48362/1441952722624/minnovative?format=1000w" />
 
-            <p>Comming soon..</p>
+            <div class="sqs-block-content">
+              <p class="text-align-center">
+                <strong>
+                  Minnovative Solutions Site Redesign - Responsive
+                </strong>
+              </p>
+              <p>
+                <strong>Problem:</strong>
+              </p>
+              <p>
+                The original minnovative site hadn't been renovated since when
+                the company was first founded, a decade prior to this project.
+                The business model had changed, and the competitors were coming
+                up with more aesthetically pleasing, easily digestible sites. I
+                had to design a site that will keep the site looking and feeling
+                up-to-date for years to come, while serving the needs of
+                potential buyers and providing all the necessary information.
+              </p>
+              <p>
+                <strong>Process:</strong>
+              </p>
+              <p>
+                I performed extensive competitive audit of the sites of the
+                competitors provided to gauge which information is the most
+                imperative to be on the site. After assessing the hierarchy of
+                information that needs to be presented in the site, I made a
+                rough wireframe or how the information should be presented on a
+                desktop browser, and on mobile.
+              </p>
+              <p>&nbsp;</p>
 
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f2771ce4b0ce0cd8d1a2b0/55f2771de4b0951c27bc4233/1441953566333/Home.jpg?format=500w" />
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f2771ce4b0ce0cd8d1a2b0/55f2771de4b0ce0cd8d1a2b2/1441953571251/Home+-+Mobile.jpg?format=500w" />
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f2771ce4b0ce0cd8d1a2b0/55f2771ee4b0ce0cd8d1a2b4/1441953566895/Suppliers.jpg?format=500w" />
+              <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f2771ce4b0ce0cd8d1a2b0/55f2771ee4b0951c27bc4235/1441953567391/Suppliers+-+Mobile.jpg?format=500w" />
+
+              <strong>Currently</strong>
+
+              <p>
+                The site is live and has increased traffic and engagement by
+                300%+.
+              </p>
+            </div>
           </div>
         </div>
         <FooterLinks />

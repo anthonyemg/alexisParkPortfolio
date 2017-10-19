f11fa7f983adeffe81316e78460d91b1fc4b4b4d
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-19 19:00:48 -0400

    [added] club pickup kiosk content

--- src/public/app/works/ClubPickupKiosk.js
+++ src/public/app/works/ClubPickupKiosk.js
@@ -1,18 +1,95 @@
 import React from 'react';
 import FooterLinks from '../components/FooterLinks';
 
-const ClubPickupKiosk= () => (
+const ClubPickupKiosk = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Club Pickup Kiosk</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f26069e4b0c0afc450726c/1441947779528/?format=1000w" />
 
-            <p>Comming soon..</p>
+            <div class="sqs-block-content">
+              <p>&nbsp;</p>
+              <p class="text-align-center">
+                <strong>Club Pickup Kiosk</strong>
+              </p>
+              <p>
+                <strong>Role:</strong> Interaction design, partial UI design
+              </p>
+              <p>
+                <strong>Problem:</strong>
+              </p>
+              <p>
+                The Club Pickup kiosk was created in order to help the members who place orders
+                online and pick it up in the store - pick up their orders even faster. For our
+                members, 24 hours in a day is not enough for them - they have family and their
+                business to take care of!&nbsp;
+              </p>
+              <p>
+                <strong>Scenario:</strong>
+              </p>
+              <p>
+                After the member places an order online, they can come in the store and “check in”
+                using the kiosk, letting an associate know that they’ve arrived. The associate is
+                alerted, then brings out the order within a few minutes and meets the member at the
+                Club Pickup area.&nbsp;
+              </p>
+              <p>
+                <strong>Process:</strong>
+              </p>
+              <p>
+                We retrieved data on order sizes, associate and member wait times, and their typical
+                behavior when ordering online or shopping in store. We also took into account what
+                kiosk device we will be using, and how it would integrate into the mental model of
+                our members.
+              </p>
+              <p>
+                <strong>1st Phase:</strong>
+              </p>
+              <p>
+                In the beginning, we started out small - the kiosk consisted of a single welcome
+                page, confirmation, and error states. Below screens show a sample of the first
+                phase.&nbsp;
+              </p>
+            </div>
 
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f26e8ae4b0c0afc4509899/55f26e8be4b0e61add8691f7/1441951373038/1Kiosk.jpg?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f26e8ae4b0c0afc4509899/55f26efee4b0fdc2e7a24090/1441951487491/2-Kiosk_02.png?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f26e8ae4b0c0afc4509899/55f26efee4b0eab704a99d4b/1441951487706/4Kiosk_02.png?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f26e8ae4b0c0afc4509899/55f26e8ce4b0f58c72711cb5/1441951372805/5Kiosk.jpg?format=300w" />
+
+            <div class="sqs-block-content">
+              <p>
+                <strong>Later Phases:</strong>
+              </p>
+              <p>
+                The home screen was updated to accommodate the scanning ability of the tablet.
+                Additional screens and features had to be created to account for multiple orders and
+                varying wait times.
+              </p>
+            </div>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560709a5e4b0da24d9266f1e/560709a6e4b013120acb1f20/1468448465623/multiple_orders.png?format=500w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560709a5e4b0da24d9266f1e/5786bf2e20099e84c631c091/1468448587919/club_pickup_kiosk.png?format=1000w" />
+
+            <div class="sqs-block-content">
+              <p>
+                <strong>Currently:</strong>
+              </p>
+              <p>
+                This is an ever-evolving project - we’re constantly finessing the flow and screens
+                to make this the most seamless experience for the members possible, and now we’re
+                launching it in our Puerto Rico clubs. So far the feedback from members has been
+                great! They love that they can pick up their order even faster now, but still get
+                the chance to see their favorite Sam’s Club associate.
+              </p>
+            </div>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f26f33e4b0d044fda11f74/1441951606074/?format=750w" />
           </div>
         </div>
         <FooterLinks />

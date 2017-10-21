a9dc8967cf9f07945da12b79fe79d7b927bd602a
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-21 18:24:21 -0400

    [added] club insider contents

--- src/public/app/works/ClubInsider.js
+++ src/public/app/works/ClubInsider.js
@@ -3,15 +3,54 @@ import FooterLinks from '../components/FooterLinks';
 
 const ClubInsider = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Club Insider</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f289a7e4b0afb1e83c1c67/1441958344512/?format=1000w" />
 
-            <p>Comming soon..</p>
+            <div class="sqs-block-content">
+              <p class="text-align-center">&nbsp;</p>
+              <p class="text-align-center">
+                <strong>Club Insider Savings Program</strong>
+              </p>
+              <p>
+                <strong>Role:</strong> Interaction design
+              </p>
+              <p>
+                <strong>Problem:</strong>
+              </p>
+              <p>
+                The existing UI and the flow of the savings program didn't allow members to directly
+                transact on the savings site. We also needed to capture the members' emails and
+                associate it with their membership number in order to provide the right information
+                and personalized experience via email. We wanted to portray a sense of exclusivity
+                by presenting this member-exclusive savings program. Furthermore, the sign-in
+                barrier needed to go away in order for users to view the savings, making it more
+                accessible for our members.
+              </p>
+              <p>
+                <strong>Process:</strong>
+              </p>
+              <p>
+                We worked on different options for presenting this screen and the savings program
+                page. We presented options that will be the most intuitive and accessible for the
+                members, ensuring that it didn't veer away too much from the existing design and
+                feel of the entire site.&nbsp;
+              </p>
+            </div>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f28c15e4b078c961791aed/55f28c16e4b0afb1e83c219d/1441958935801/clubinsiderwf1.png?format=500w"/>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/55f28c15e4b078c961791aed/55f28c16e4b0afb1e83c219c/1441958936071/clubinsiderwf2.png?format=500w"/>
+
+
+            <div class="sqs-block-content"><p id="yui_3_17_2_46_1441954721581_19439"><strong>Currently:</strong></p><p id="yui_3_17_2_46_1441954721581_20428">We were able to increase member engagement with our discount program online. We were also able to&nbsp;capture and associate their email for members who were previously not engaged online. The savings items are not actionable and can be directly added to cart, which was not possible previously. We're watching analytics to see what improvements we can make in the next iteration.</p><p id="yui_3_17_2_46_1441954721581_21076"><br></p></div>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f28beee4b07b1c9b74ae0b/1441958898244/?format=1000w"/>
+            
 
           </div>
         </div>

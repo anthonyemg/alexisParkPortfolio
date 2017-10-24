40be37ad87eadddec908da27c2587a0cfba88b56
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-23 23:47:43 -0400

    [added] content for anthology

--- src/public/app/works/Anthology.js
+++ src/public/app/works/Anthology.js
@@ -3,16 +3,100 @@ import FooterLinks from '../components/FooterLinks';
 
 const Anthology = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Anthology</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
-
-            <p>Comming soon..</p>
-
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/55f280eae4b0c0afc450c27e/1441956091363/?format=1000w" />
+            <div class="sqs-block-content">
+              <p class="text-align-center">
+                <strong>Anthology </strong>
+              </p>
+              <p>
+                <strong>Role:</strong> Research, interaction design, UI design
+              </p>
+              <p>
+                <strong>Problem:</strong>
+              </p>
+              <p>
+                The unique model of Anthology's recruiting style requires a somewhat lengthy sign up
+                process for a potential new user. How can we make this feel less cumbersome and
+                intuitive?
+              </p>
+              <p>
+                <strong>Process:</strong>
+              </p>
+              <p>
+                <strong>1) UX audit and usability testing</strong>
+              </p>
+              <p>
+                Performed an app and mweb, desktop website-wide audit to assess current UI and UX
+                issues. Conducted usability testing to find out the initial response of brand-new
+                users.&nbsp;Recommended changes based upon the findings. Below pages are a sample of
+                the audit doc delivered.
+              </p>
+            </div>
+          </div>
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b8568e4b081ffdfe73962/1443595624872/Poachable+UX+Audit.002.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b8569e4b0a43ccfcf6c4d/1443595626595/Poachable+UX+Audit.003.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b8569e4b081ffdfe73976/1443595626683/Poachable+UX+Audit.004.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b856ae4b0a43ccfcf6c5f/1443595627460/Poachable+UX+Audit.005.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b856be4b081ffdfe73979/1443595628173/Poachable+UX+Audit.006.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b856de4b0a43ccfcf6c61/1443595629964/Poachable+UX+Audit.007.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b856de4b081ffdfe7397c/1443595630092/Poachable+UX+Audit.036.jpg?format=300w" />
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b8566e4b0651fc5570f94/560b856fe4b0a43ccfcf6c76/1443595632360/Poachable+UX+Audit.037.jpg?format=300w" />
+          <div
+            class="sqs-block html-block sqs-block-html"
+            data-block-type="2"
+            id="block-yui_3_17_2_10_1443472517163_18332"
+          >
+            <div class="sqs-block-content">
+              <p>
+                Since I had limited timeframe, I conducted 5 usability tests using friends and
+                colleagues to get their feedback on the current on-boarding process on desktop and
+                the app. The main feedback provided were:
+              </p>
+              <p>
+                a) The process seems daunting and there's no clear explanation on why this needs to
+                be done (the on-boarding process consists of 30+ pages).
+              </p>
+              <p>
+                b) Once the process is completed, there is no instant reward or gratification for
+                completing the lengthy process.&nbsp;
+              </p>
+              <p>
+                <strong>2) Personas</strong>
+              </p>
+              <p>
+                Then.&nbsp;I created two quick and dirty main personas of Anthology's user base so
+                it'll help me explore these screens in their mindset, and also to help me discuss my
+                decisions with my client.
+              </p>
+            </div>
+          </div>
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/560b90e0e4b0a1d8c245ed78/560b90e4e4b067a54c35999c/1443598567522/anthology_personas.png?format=750w" />
+          <p>
+            Then I created a new flow with updated screens in Omnigraffle to test out in a quick
+            Flinto prototype.
+          </p>
+          <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/560b88aae4b0a43ccfcf78c7/1443596461462/?format=1000w" />
+          <p>Wireframe sample</p>
+          <div class="sqs-block-content">
+            <p>
+              <strong>Currently:</strong>
+            </p>
+            <p>
+              Anthology has updated the app on-boarding process and screens per my recommendations,
+              and has seen a positive impact on the completion rate (5.2% increase - what was
+              surprising before was that although the on-boarding process was cumbersome, the
+              success rate was already at 77%). I'm working with Anthology on multiple projects on a
+              regular basis to make the perfect match between a candidate and an employer - and
+              eventually to change how people find the best job of their life.&nbsp;
+            </p>
+            <p>&nbsp;</p>
           </div>
         </div>
         <FooterLinks />

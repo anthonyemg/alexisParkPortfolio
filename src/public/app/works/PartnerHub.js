6e55aaf14bbda0dca8b9b2cfbb83d8e36ccadf75
Anthony Greenheck <aemgreenheck@gmail.com>
2017-10-25 00:07:07 -0400

    [added] partner hub content

--- src/public/app/works/PartnerHub.js
+++ src/public/app/works/PartnerHub.js
@@ -3,16 +3,155 @@ import FooterLinks from '../components/FooterLinks';
 
 const PartnerHub = () => (
   <div>
-    <div className='page-container'>
-      <div className='works-wrapperTop'>
+    <div className="page-container">
+      <div className="works-wrapperTop">
         <span>Partner Hub</span>
       </div>
-      <div className='page-wrapperBottom'>
-        <div className='worksTemplate-bottomText page-bottomText'>
+      <div className="page-wrapperBottom">
+        <div className="worksTemplate-bottomText page-bottomText">
           <div>
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/t/577de3dbbebafbf9baba8d72/1467868142967/?format=750w" />
 
-            <p>Comming soon..</p>
+            <div class="sqs-block-content">
+              <p class="text-align-center">
+                <strong>Partner Hub Redesign</strong>
+              </p>
+              <p>
+                <strong>Role:</strong>&nbsp;Oversee design direction, Interaction design, UI design
+              </p>
+              <p>
+                <strong>Objective:</strong>
+              </p>
+              <p>
+                The Starbucks partner hub (partner.starbucks.com)&nbsp;is used daily by partners
+                (employees) in the HQ building, but most importantly,&nbsp;retail partners
+                (in-store) across the country and Canada. It has plethora of information pertaining
+                to partner info, pay, benefits, company news, and store resources and alerts.
+              </p>
+              <p>
+                It was launched 7 years ago on Sharepoint platform, and was in a dire need of a
+                design uplift. Some work to make it responsive was done a while ago, but many
+                functionalities were broken.&nbsp;Aside from the cosmetic needs, the information
+                architecture of the whole site was up to many individual contributors, which made it
+                very difficult to navigate through. The end goal of the project wasn't only to make
+                the site pretty, but to reconstruct the navigational features to surface topics that
+                are the most important to our retail partners, and make it as accessible as
+                possible.
+              </p>
+              <p>
+                <strong>Process:</strong>
+              </p>
+              <p>
+                The starbucks brand team prior to this has provided the partner team with a high
+                level design guide for the new partner digital look and feel. The initial ideation
+                phase was done by a local agency - I oversaw the process with the partner digital
+                product team to see that the design was headed to the right direction, the visual
+                elements were in line with what the brand team had initially envisioned, and if the
+                interactions of the new feature functionalities made sense. Also, once the ideation
+                deliverables were handed off, I worked closely with the engineering team and a team
+                of analysts to fill in the gaps.&nbsp;
+              </p>
+              <p>
+                The biggest problem with the old design was that retail partners - especially store
+                managers and shift managers - who are consistently using the site on a daily basis,
+                were having trouble getting to the most important information. For example, they
+                were taking several clicks to get to the info they need, or didn't know that certain
+                buttons were buttons, and were constantly calling the help desk or neighboring
+                stores to find out where the information they needed was.&nbsp;
+              </p>
+              <p>
+                I took the feedback that's been taken the most, and also watched and interviewed 10
+                different store managers &amp; shift managers on how they go about using the partner
+                site.
+              </p>
+              <p>
+                <strong>Prior to redesign:</strong>
+              </p>
+              <p>You can click on each screen to see them in detail.&nbsp;</p>
+            </div>
 
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef9d715d5dbbc19b6d430/577ef9da03596e7a36013104/1467939414400/Screen+Shot+2016-04-25+at+11.28.03+AM.png?format=300w" />
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef9d715d5dbbc19b6d430/577ef9dad1758ec8236a78f3/1467939420699/mobile_home.png?format=300w" />
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef9d715d5dbbc19b6d430/577ef9ded1758ec8236a7913/1467939416914/Screen+Shot+2016-05-17+at+2.14.47+PM.png?format=300w" />
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef9d715d5dbbc19b6d430/577ef9d703596e7a360130e6/1467939411596/article.png?format=300w" />
+            <p>
+              <strong>Iterations:</strong>
+            </p>
+
+            <p>You can click on each screen to see them in detail.&nbsp;</p>
+
+            <p>
+              <strong>Beginning phase</strong>
+            </p>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577df06f725e25fb8efc368d/577ef4228419c26fee6ae79f/1468298885768/ver1_store.jpg?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577df06f725e25fb8efc368d/577ef42c6b8f5b3cacc2d1c8/1467937838816/ver1_yourhub.jpg?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577df06f725e25fb8efc368d/577ef4226b8f5b3cacc2d17e/1467937851688/ver1_partner.png?format=100w" />
+
+            <p>
+              <strong>Later phase</strong>
+            </p>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef6d6414fb5b2350aebc8/577ef6d637c581ee1a133b97/1467938526166/ver2_companyreal.jpg?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef6d6414fb5b2350aebc8/577ef6d6ebbd1af4c46a985e/1467938526732/ver2_company.jpg?format=300w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577ef6d6414fb5b2350aebc8/577ef6da37c581ee1a133bb8/1467938530961/ver2_partner.jpg?format=100w" />
+
+            <p>
+              One thing that changed significantly throughout the phases is the information
+              architecture of partner hub. I conducted some unmoderated, open card sorts, and some
+              moderated, closed card sorts with about 30 partners across the country using Userzoom,
+              and with 6 partners in person to see how they would go about logically rearranging the
+              hierarchy of some pages, and how they would go about re-naming some of the categories
+              ("What's 'business acumen'?).
+            </p>
+
+            <p>
+              <strong>Latest:</strong>
+            </p>
+
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577f03452994caa7930d4728/5784766029687fbea45c22b4/1468298853073/current_home.png?format=500w" />
+            <img src="https://static1.squarespace.com/static/55c2e779e4b0c20768545b47/577f03452994caa7930d4728/577f4ba437c581ee1a156f8d/1467960237057/PH_FLC_Phase2.jpg?format=500w" />
+
+            <div
+              class="sqs-block html-block sqs-block-html"
+              data-block-type="2"
+              id="block-yui_3_17_2_29_1467853979938_72937"
+            >
+              <div class="sqs-block-content">
+                <p>
+                  In the end, because sbux will be moving platform the site is built on at the end
+                  of the fiscal year, we had to park a lot of the new designs as future stories.
+                  Some immmediate solves we were able to implement were tackling a few of the
+                  biggest issues.
+                </p>
+                <p>
+                  • For in-store partners, we surfaced store news and resources first, and surfaced
+                  the most-looked up links to the top/above the fold, which decreased the number of
+                  calls by 26% upon launch.
+                </p>
+                <p>
+                  • Alert functionality for quickly communicating system downs or natural disasters
+                </p>
+                <p>• Entire site got a new look and feel.</p>
+                <p>
+                  <strong>Takeaways:</strong>
+                </p>
+                <p>
+                  There are many difficulties I ran into while working on this redesign. In the
+                  beginning of the phase, I came up with a lot of &nbsp;ideas that quickly turned
+                  out to be very difficult to implement. I had to have many discussions with the
+                  development team to fully grasp how the functionalities were originally built. In
+                  the end, we discussed how we can improve the current functionalities as much as we
+                  can, while also providing a brand uplift. We're currently working on testing the
+                  final proposed designs. Constant communication with all cross-functional teams is{' '}
+                  <strong>(insert key emoji here)</strong>
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
           </div>
         </div>
         <FooterLinks />
